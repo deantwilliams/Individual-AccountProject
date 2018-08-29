fde1ccf5ef10cb7d3254d085097f842dd57f45ca
deantwilliams <dean.t.williams@outlook.com>
2018-08-29 14:01:20 +0100

    Added backend springboot functionality to basic level

--- src/main/java/com/qa/account/AccountProject/controllers/AccountsController.java
+++ src/main/java/com/qa/account/AccountProject/controllers/AccountsController.java
@@ -2,13 +2,7 @@ package com.qa.account.AccountProject.controllers;
 
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,38 +13,56 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.account.AccountProject.models.Account;
-import com.qa.util.JSONUtil;
+import com.qa.account.repositories.AccountRepository;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
 public class AccountsController {
-
-	@PersistenceContext
-	private EntityManager em;
 	
-	@Inject
-	private JSONUtil util;
+	@Autowired
+	private AccountRepository accountRepo;
 	
 	@GetMapping
-	public String getAllAccounts()
+	public List<Account> getAllAccounts()
 	{
-		TypedQuery<Account> query = em.createQuery("Select a FROM Account a ORDER m.id DESC",Account.class);
-		List<Account> accounts = (List<Account>) query.getResultList();
-		return util.convertObjectToJSON(accounts);
+		return accountRepo.findAll();
 	}
 	
 	@PostMapping
-	@Transactional(TxType.REQUIRED)
-	public String createAccount(String account)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createAccount(@RequestBody Account account)
 	{
-		Account acc = util.convertJSONToObject(account, Account.class);
-		em.persist(acc);
-		return "Account has been added";
+		accountRepo.save(account);
+	}
+	
+	@PostMapping("/{id}")
+	@ResponseStatus(HttpStatus.GONE)
+	public void deleteAccount(@PathVariable("id") long id)
+	{
+		accountRepo.deleteById(id);
+	}
+	
+	@PostMapping("/update")
+	@ResponseStatus(HttpStatus.OK)
+	public void updateAccount(@RequestBody Account accountUpdated)
+	{
+		Account accountToUpdate = getSingleAccount(accountUpdated.getId());
+		if(accountToUpdate != null)
+		{
+			accountUpdated.setId(accountToUpdate.getId());
+			accountRepo.save(accountUpdated);
+		}
+		
 	}
 	
 	@GetMapping("/{id}")
 	public Account getAccount(@PathVariable("id") long id)
 	{
-		return new Account();
+		return getSingleAccount(id);
+	}
+	
+	private Account getSingleAccount(long id)
+	{
+		return accountRepo.getOne(id);
 	}
 }
